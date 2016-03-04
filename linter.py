2bab6cbe6c633fde006bb69e7a1a921493394e3c
Jeremy Hanson-Finger <jeremy.hansonfinger@shopify.com>
2016-03-04 14:04:22 -0500

    initial commit

---
+++ linter.py
@@ -0,0 +1,31 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Sindre Sorhus
+# Copyright (c) 2015 Sindre Sorhus
+#
+# License: MIT
+#
+
+from SublimeLinter.lint import NodeLinter
+
+
+class Rory(NodeLinter):
+    cmd = 'rory'
+    regex = r'^\s+(?P<line>\d+):(?P<col>\d+).+(?:(?P<warning>warning)) (?P<message>.+)'
+    selectors = {
+        'html': 'text.html.basic'
+    }
+    syntax = (
+        'markdown',
+        'markdown extended',
+        'multimarkdown',
+        'plain text',
+        'ruby',
+        'html (rails)',
+        'html'
+    )
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>=0.0.0'
